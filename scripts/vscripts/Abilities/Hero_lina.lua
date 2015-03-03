036788e5a50fbfd401cfcd4d65c3d5469fad2d51
a348730140 <a348730140@163.com>
2015-03-03 19:19:31 +0800

    莉娜的第一次技能

--- scripts/vscripts/Abilities/Hero_lina.lua
+++ scripts/vscripts/Abilities/Hero_lina.lua
@@ -1,113 +1,134 @@
 
 
 --弹跳火球
+
 function LinaOneAbility1( keys )
 	local caster = keys.caster
-	local target = keys.target
-	local old_target = caster
+ 	local target = keys.target
+ 	local ability = keys.ability
+ 	local effectName = keys.EffectName
+ 	local move_speed = tonumber(keys.move_speed)
+ 	local radius = keys.radius
+ 	local count = keys.count
+ 	local teams = ability:GetAbilityTargetTeam()
+    local types = ability:GetAbilityTargetType()
+    local flags = ability:GetAbilityTargetFlags()
+    local find_tpye = FIND_CLOSEST
 
-	local str = DoUniqueString("LinaOneAbility1")
-	print(str)
-
-	local unit = CreateUnitByName("npc_majia",caster:GetOrigin(),false,nil,nil,caster:GetTeamNumber())
-	unit.LinaOneAbility1Next = false
-	unit.LinaOneAbility1ThisProjectile 	= str
-	unit.LinaOneAbility1ThisTarget		= target
-
-	if caster.LinaOneAbility1MaJia == nil then
-		caster.LinaOneAbility1MaJia = {}
-	end
-
-	table.insert(caster.LinaOneAbility1MaJia,unit)
-
-	local teams = DOTA_UNIT_TARGET_TEAM_ENEMY
-    local types = DOTA_UNIT_TARGET_BASIC+DOTA_UNIT_TARGET_HERO
-    local flags = DOTA_UNIT_TARGET_FLAG_NONE
-
-    local fire = true
-    local num = 0
-	GameRules:GetGameModeEntity():SetContextThink(str,
-		function( )
-
-			if unit.LinaOneAbility1Next then
-				local group = FindUnitsInRadius(caster:GetTeamNumber(),target:GetOrigin(),nil,600,teams,types,flags,FIND_CLOSEST,true)
-
-				for i=1,#group do
-					if group[i].LinaOneAbility1Impact == nil then
-						group[i].LinaOneAbility1Impact = {}
-					end
-					if LinaOneAbility1FindImpact(group[i],str) == false then
-						old_target = target
-						target = group[i]
-						fire = true
-						unit.LinaOneAbility1Next =false
-						unit.LinaOneAbility1ThisTarget = target
-						break
-					end
-				end
-
-				if unit.LinaOneAbility1ThisTarget == target then
-					PrintMsg(num)
-					PrintMsg("over")
-					TableRemoveTable(caster.LinaOneAbility1MaJia,unit)
-					unit:RemoveSelf()
-					return nil
-				end
-			end
-
-			if fire then
-				fire = false
-				local info = 
-			    {
-			        Target = target,
-			        Source = old_target,
-			        Ability = keys.ability,  
-			        EffectName = "particles/units/heroes/hero_jakiro/jakiro_base_attack_fire.vpcf",
-			        bDodgeable = false,
-			        iMoveSpeed = 1100,
-			        bProvidesVision = true,
-			        iVisionRadius = 300,
-			        iVisionTeamNumber = caster:GetTeamNumber(),
-			        iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
-			    }
-			    projectile = ProjectileManager:CreateTrackingProjectile(info)
-			    num = num + 1
-			end
-
-		    return 0.05
-		end,0)
+ 	Catapult( caster,target,ability,effectName,move_speed,radius,count,teams,types,flags,find_tpye )
 end
 
-function LinaOneAbility1FindImpact( unit,str )
+-- function LinaOneAbility1( keys )
+-- 	local caster = keys.caster
+-- 	local target = keys.target
+-- 	local old_target = caster
+
+-- 	local str = DoUniqueString("LinaOneAbility1")
+
+-- 	local unit = {}
+-- 	unit.LinaOneAbility1Next = false
+-- 	unit.LinaOneAbility1ThisProjectile 	= str
+-- 	unit.LinaOneAbility1ThisTarget		= target
+
+-- 	if caster.LinaOneAbility1MaJia == nil then
+-- 		caster.LinaOneAbility1MaJia = {}
+-- 	end
+
+-- 	table.insert(caster.LinaOneAbility1MaJia,unit)
+
+-- 	local teams = DOTA_UNIT_TARGET_TEAM_ENEMY
+--     local types = DOTA_UNIT_TARGET_BASIC+DOTA_UNIT_TARGET_HERO
+--     local flags = DOTA_UNIT_TARGET_FLAG_NONE
+
+--     local fire = true
+--     local count = 0
+    
+-- 	GameRules:GetGameModeEntity():SetContextThink(str,
+-- 		function( )
+-- 			if count>=keys.count then
+-- 				return nil
+-- 			end
+
+
+-- 			if unit.LinaOneAbility1Next then
+-- 				local group = FindUnitsInRadius(caster:GetTeamNumber(),target:GetOrigin(),nil,keys.radius,teams,types,flags,FIND_CLOSEST,true)
+				
+-- 				local num = 0
+-- 				for i=1,#group do
+-- 					if group[i].LinaOneAbility1Impact == nil then
+-- 						group[i].LinaOneAbility1Impact = {}
+-- 					end
+
+-- 					local impact = LinaOneAbility1FindImpact(group[i],str)
+
+-- 					if  impact == false then
+-- 						old_target = target
+-- 						target = group[i]
+-- 						fire = true
+-- 						unit.LinaOneAbility1Next =false
+-- 						unit.LinaOneAbility1ThisTarget = target
+-- 						break
+-- 					end
+-- 					num = num + 1
+-- 				end
+
+-- 				if num >= #group then
+-- 					TableRemoveTable(caster.LinaOneAbility1MaJia,unit)
+-- 					return nil
+-- 				end
+-- 			end
+
+-- 			if fire then
+-- 				fire = false
+-- 				count = count + 1
+-- 				local info = 
+-- 			    {
+-- 			        Target = target,
+-- 			        Source = old_target,
+-- 			        Ability = keys.ability,  
+-- 			        EffectName = "particles/units/heroes/hero_jakiro/jakiro_base_attack_fire.vpcf",
+-- 			        bDodgeable = false,
+-- 			        iMoveSpeed = 1100,
+-- 			        bProvidesVision = true,
+-- 			        iVisionRadius = 300,
+-- 			        iVisionTeamNumber = caster:GetTeamNumber(),
+-- 			        iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
+-- 			    }
+-- 			    projectile = ProjectileManager:CreateTrackingProjectile(info)			    
+-- 			end
+
+-- 		    return 0.05
+-- 		end,0)
+-- end
+
+-- function LinaOneAbility1FindImpact( unit,str )
 	
-	for i,v in pairs(unit.LinaOneAbility1Impact) do
-		if v == str then
-			return true
-		end
-	end
-	return false
-end
+-- 	for i,v in pairs(unit.LinaOneAbility1Impact) do
+-- 		if v == str then
+-- 			return true
+-- 		end
+-- 	end
+-- 	return false
+-- end
 
-function LinaOneAbility1Impact( keys )
-	local caster = keys.caster
-	local target = keys.target
+-- function LinaOneAbility1Impact( keys )
+-- 	local caster = keys.caster
+-- 	local target = keys.target
 
-	if target.LinaOneAbility1Impact == nil then
-		target.LinaOneAbility1Impact = {}
-	end
+-- 	if target.LinaOneAbility1Impact == nil then
+-- 		target.LinaOneAbility1Impact = {}
+-- 	end
 
-	print("LinaOneAbility1Impact")
-	for i,v in pairs(caster.LinaOneAbility1MaJia) do
+-- 	for i,v in pairs(caster.LinaOneAbility1MaJia) do
 		
-		if v.LinaOneAbility1ThisProjectile ~= nil and v.LinaOneAbility1ThisTarget ~= nil then
+-- 		if v.LinaOneAbility1ThisProjectile ~= nil and v.LinaOneAbility1ThisTarget ~= nil then
 
-			if v.LinaOneAbility1ThisTarget == target then
-				table.insert(target.LinaOneAbility1Impact,v.LinaOneAbility1ThisProjectile)
-				print("target.."..v.LinaOneAbility1ThisProjectile)
-				v.LinaOneAbility1Next = true
-				return
-			end
+-- 			if v.LinaOneAbility1ThisTarget == target then
+-- 				table.insert(target.LinaOneAbility1Impact,v.LinaOneAbility1ThisProjectile)
+-- 				v.LinaOneAbility1Next = true
+-- 				return
+-- 			end
 
-		end
-	end
-end
+-- 		end
+-- 	end
+-- end
--- scripts/vscripts/Abilities/common.lua
+++ scripts/vscripts/Abilities/common.lua
@@ -75,3 +75,177 @@ function SyncAbilityLevel( keys )
 		end
 	end
 end
+
+--弹射函数
+--用于检测是否被此次弹射命中过
+function CatapultFindImpact( unit,str )
+	for i,v in pairs(unit.CatapultImpact) do
+		if v == str then
+			return true
+		end
+	end
+	return false
+end
+
+--caster是施法者或者主要来源
+--target是第一个目标
+--ability是技能来源
+--effectName是弹射的投射物
+--move_speed是投射物的速率
+--doge是表示能否被躲掉
+--radius是每次弹射的范围
+--count是弹射次数
+--teams,types,flags获取单位的三剑客
+--find_tpye是单位组按远近或者随机排列
+--		FIND_CLOSEST
+--      FIND_FARTHEST
+--      FIND_UNITS_EVERYWHERE
+function Catapult( caster,target,ability,effectName,move_speed,radius,count,teams,types,flags,find_tpye )
+	print("Run Catapult")
+
+	local old_target = caster
+
+	--生成独立的字符串
+	local str = DoUniqueString(ability:GetAbilityName())
+	print("Catapult:"..str)
+
+	--假设一个马甲
+	local unit = {}
+
+	--绑定信息
+	--是否发射下一个投射物
+	unit.CatapultNext = false
+
+	--本次弹射标识的字符串
+	unit.CatapultThisProjectile = str
+
+	--本次弹射的目标
+	unit.CatapultThisTarget		= target
+
+	--CatapultUnit用来存储unit
+	if caster.CatapultUnit == nil then
+		caster.CatapultUnit = {}
+	end
+
+	--把unit插入CatapultUnit
+	table.insert(caster.CatapultUnit,unit)
+
+	--用于决定是否发射投射物
+    local fire = true
+
+    --弹射最大次数
+    local count_num = 0
+    
+	GameRules:GetGameModeEntity():SetContextThink(str,
+		function( )
+
+			--满足达到最大弹射次数删除计时器
+			if count_num>=count then
+				print("Catapult impact :"..count_num)
+				print("Catapult:"..str.." is over")
+				return nil
+			end
+
+
+			if unit.CatapultNext then
+
+				--获取单位组
+				local group = FindUnitsInRadius(caster:GetTeamNumber(),target:GetOrigin(),nil,radius,teams,types,flags,FIND_CLOSEST,true)
+				
+				--用于计算循环次数
+				local num = 0
+				for i=1,#group do
+					if group[i].CatapultImpact == nil then
+						group[i].CatapultImpact = {}
+					end
+
+					--判断是否命中
+					local impact = CatapultFindImpact(group[i],str)
+
+					if  impact == false then
+
+						--替换old_target
+						old_target = target
+
+						--新target
+						target = group[i]
+
+						--可以发射新投射物
+						fire = true
+
+						--等待下一个目标
+						unit.CatapultNext =false
+
+						--锁定当前目标
+						unit.CatapultThisTarget = target
+						break
+					end
+					num = num + 1
+				end
+
+				--如果大于等于单位组的数量那么就删除计时器
+				if num >= #group then
+					--从CatapultUnit中删除unit
+					TableRemoveTable(caster.CatapultUnit,unit)
+
+					print("Catapult impact :"..count_num)
+					print("Catapult:"..str.." is over")
+					return nil
+				end
+			end
+
+			--发射投射物
+			if fire then
+				fire = false
+				count_num = count_num + 1
+				local info = 
+			    {
+			        Target = target,
+			        Source = old_target,
+			        Ability = ability,  
+			        EffectName = effectName,
+			        bDodgeable = false,
+			        iMoveSpeed = move_speed,
+			        bProvidesVision = true,
+			        iVisionRadius = 300,
+			        iVisionTeamNumber = caster:GetTeamNumber(),
+			        iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
+			    }
+			    projectile = ProjectileManager:CreateTrackingProjectile(info)			    
+			end
+
+		    return 0.05
+		end,0)
+end
+
+--此函数在KV里面用OnProjectileHitUnit调用
+function CatapultImpact( keys )
+	local caster = keys.caster
+	local target = keys.target
+
+	--防止意外
+	if caster.CatapultUnit == nil then
+		caster.CatapultUnit = {}
+	end
+	if target.CatapultImpact == nil then
+		target.CatapultImpact = {}
+	end
+
+	--挨个检测是否是弹射的目标
+	for i,v in pairs(caster.CatapultUnit) do
+		
+		if v.CatapultThisProjectile ~= nil and v.CatapultThisTarget ~= nil then
+
+			if v.CatapultThisTarget == target then
+
+				--标记target被CatapultThisProjectile命中
+				table.insert(target.CatapultImpact,v.CatapultThisProjectile)
+
+				--允许发射下一次投射物
+				v.CatapultNext = true
+				return
+			end
+
+		end
+	end
+end
